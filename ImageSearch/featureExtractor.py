35892b7eda02e19659919228436a3a563eea3b3e
Ravi Rane <ravi_rane20@yahoo.com>
2019-07-23 07:35:42 -0400

    search model change

--- ImageSearch/featureExtractor.py
+++ ImageSearch/featureExtractor.py
@@ -14,6 +14,7 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.optimizers import SGD, Adam
 import sys
+from keras.models import Model, load_model
 
 
 class FeatureExtractor:
@@ -50,19 +51,19 @@ class FeatureExtractor:
         # self.model = model
         # self.opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
         # self.graph = tf.get_default_graph()
-        base_model = VGG16(weights=None)
-        base_model.load_weights('/scratch/rrane/data/vgg16_weights.h5')
-        self.model = Model(inputs=base_model.input, outputs=base_model.get_layer('fc1').output)
+        model = load_model('/Users/ravirane/Desktop/GMU/DAEN690/CNN/model_checkpnt.hdf5')
+        self.feature_model = Model(inputs=model.input,
+                                 outputs=model.get_layer('feature_dense').output)
         self.graph = tf.get_default_graph()
         
 
     def extract(self, img):  # img is from PIL.Image.open(path) or keras.preprocessing.image.load_img(path)
-        img = img.resize((224, 224))  # VGG must take a 224x224 img as an input
+        img = img.resize((64, 64))  # VGG must take a 224x224 img as an input
         img = img.convert('RGB')  # Make sure img is color
         x = image.img_to_array(img)  # To np.array. Height x Width x Channel. dtype=float32
         x = np.expand_dims(x, axis=0)  # (H, W, C)->(1, H, W, C), where the first elem is the number of img
         x = preprocess_input(x)  # Subtracting avg values for each pixel
 
         with self.graph.as_default():
-            feature = self.model.predict(x)[0]  # (1, 4096) -> (4096, )
+            feature = self.feature_model.predict(x)[0]  # (1, 4096) -> (4096, )
             return feature / np.linalg.norm(feature)  # Normalize
